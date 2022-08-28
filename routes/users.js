5a94041265f60c00ad1c2f6c1100708f6e3bda15
ClaraPons <claragapons@gmail.com>
2022-08-28 19:31:52 +0200

    modifications

--- routes/users.js
+++ routes/users.js
@@ -19,11 +19,11 @@ app.post('/',
     body('name').isLength({min: 4}).withMessage("Name length isn't right"),
     body('password').isLength({min: 8}).withMessage("Password length isn't right"),
     body('city').isIn(cities).withMessage("City isn't in the list"),
-    body('email').isEmail().withMessage("this isn't a email"),
+    body('email').isEmail().withMessage("Email isn't right"),
     (req, res) => {
         const { errors } = validationResult(req)
         // console.log(req.body);
-
+        
         const user = {
             ...req.body,
             slug: slugify(req.body.name, {lower: true}) 
